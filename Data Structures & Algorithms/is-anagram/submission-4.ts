6f053345a3b2ed8eac93f8dd3ea81deb4030a76a
Alisa Bondar <alisa.k.bondar@gmail.com>
2026-08-12 12:16:23 -0600

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.ts
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        // counting pattern - need a map
+        // time O(m+n) aka O(n)
+        // space O(n)
+        if (s.length !== t.length) return false
+
+        let sMap: Map<string, number> = new Map()
+        for (const char of s) {
+            let current = sMap.get(char)
+            if (current) {
+                sMap.set(char, current + 1)
+            } else sMap.set(char, 1)
+        }
+
+        for (const ch of t) {
+            // subtract and if 0 is found then return false
+            let current = sMap.get(ch)
+            if (current === undefined || current === 0) return false;
+            sMap.set(ch, current - 1)
+        }
+
+        return true;
+    }
+}
